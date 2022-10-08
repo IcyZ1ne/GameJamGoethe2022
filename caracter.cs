4a27c2977f05f36fa44e352c7c52deddcab64e47
IcyZ1ne <andispetrus97@yahoo.com>
2022-10-08 13:24:20 +0300

    Third commit

--- caracter.cs
+++ caracter.cs
@@ -3,19 +3,27 @@ using System;
 
 public class caracter : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-	// private 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		Vector2 velocity = new Vector2();
+		if (Input.IsActionPressed("ui_left")) {
+			velocity.x -= 150;
+		}
+		if (Input.IsActionPressed("ui_right")) {
+			velocity.x += 150;
+		}
+		if (Input.IsActionPressed("ui_up")) {
+			velocity.y -= 150;
+		}
+		if (Input.IsActionPressed("ui_down")) {
+			velocity.y += 150;
+		}
+		MoveAndSlide(velocity,Vector2.Up);
+	}
 }
